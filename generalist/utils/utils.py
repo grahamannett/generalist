5711d4703abca67c2ba18fffed1673974d385df8
graham <graham.annett@gmail.com>
2022-07-26 14:23:10 +0300

    ignore all ignore folders

--- generalist/utils/utils.py
+++ generalist/utils/utils.py
@@ -12,7 +12,14 @@ def get_args():
     parser.add_argument("--datasets", nargs="+", type=str, default=["xsum"])
     parser.add_argument("-bs", "--batch_size", type=int, default=8, dest="batch_size")
     parser.add_argument("--n_epochs", type=int, default=1, dest="n_epochs")
+    parser.add_argument("--display", action=argparse.BooleanOptionalAction)
     return parser.parse_args()
+
+
+def get_hostname():
+    import platform
+
+    return platform.node()
 
 
 def matplotlib_system_setup():
