61fb9428a62920a82045fab0cd129b1c34bb38c0
graham <graham.annett@gmail.com>
2022-08-11 13:25:50 +0300

    tests for image tokenizer

---
+++ tests/test_images.py
@@ -0,0 +1,28 @@
+import unittest
+import torch
+from generalist.generalist_tokenizers.image_tokenizer import ImageTokenizer
+
+from generalist.generalist_tokenizers.input_types import ImageType
+from generalist.generalist_embedding.image_embedding import ImageEmbeddingPath, ImagePath
+
+
+class TestImages(unittest.TestCase):
+    def setUp(self):
+        self.image_tokenizer = ImageTokenizer()
+        self.image_embedder = ImagePath()
+
+    def test_tokenizer(self):
+        # 3x224x224
+        image_tensor = torch.rand(3, 224, 224)
+        image = ImageType(image_tensor)
+        tokenized = self.image_tokenizer(image)
+        self.assertEqual(tokenized.shape, torch.Size([1, 196, 768]))
+
+        # 3x320x320
+        image_tensor = torch.rand(3, 320, 320)
+        image = ImageType(image_tensor)
+        tokenized = self.image_tokenizer(image)
+        self.assertEqual(tokenized.shape, torch.Size([1, 400, 768]))
+
+    def test_embedding(self):
+        pass
