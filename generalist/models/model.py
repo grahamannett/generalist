c9a3e596906b03dba8d5db5de407aa4e560ea99f
graham <graham.annett@gmail.com>
2022-09-22 13:14:02 -0600

    20220922131401

--- generalist/models/model.py
+++ generalist/models/model.py
@@ -19,22 +19,19 @@ class GeneralistModel(nn.Module):
         **kwargs,
     ) -> None:
         super().__init__()
+        self.token_idx = token_idx
+        self.embed_dim = embed_dim
 
-        self.output_model = output_model
         if embedding_model is not None:
             raise TypeError("Embedding model should be moved outside of Generalist Model")
-        self.embedding_model = embedding_model
 
+        self.output_model = output_model
+        self.embedding_model = embedding_model
         self.transformer = Transformer()
-        # self.transformer = TransformerDecoder(
-        #     n_layer=4, embed_dim=embed_dim, num_heads=8, attn_pdrop=0.1, resid_pdrop=0.1, block_size=1024
-        # )
 
         self.model_max_length = self.transformer.model_max_length
-        self.token_idx = token_idx
 
     def forward(self, embedded: torch.Tensor, embedded_target: torch.Tensor = None, **kwargs) -> torch.Tensor:
-        # breakpoint()
         hidden_states = self.transformer(embedded=embedded, embedded_target=embedded_target)
         out = self.output_model(hidden_states, decoder_query=embedded_target)
         return out
